cdd4767c1cecf328bf226c38777e5cce611ce184
Sergey Zhukov <sergey@jetbrains.com>
2015-03-15 19:19:38 +0300

    Fix of strage problem on Docker hub

--- pre-run.sh
+++ pre-run.sh
@@ -7,6 +7,7 @@ fi
 if [[ ! -z "${SMART_HOST}" ]]; then
     echo "define(\`SMART_HOST', \`${SMART_HOST}')" >> /etc/mail/sendmail.mc
     cd /etc/mail/
+    rm -f access.db
     m4 sendmail.mc > sendmail.cf
     makemap hash access < access
 fi
